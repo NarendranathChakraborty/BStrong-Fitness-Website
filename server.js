a4e57062e5fe0ca13d620a31cb787ca22da5ed6b
SWAROOPBHAGWAT <swaroopbhagwat369@gmail.com>
2023-10-05 18:39:37 +0530

    added backend code and signup page

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express();
-const PORT = process.env.PORT || 3000; // Change the port number
 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 // Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://swaroop:Sambhaji2@cluster0.t5xti2g.mongodb.net/', {
+mongoose.connect('mongodb+srv://321swaroop0062:Sambhaji2@cluster0.xk2sux7.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log('MongoDB Atlas connected successfully.');
 }).catch((error) => {
-
-
   console.error('MongoDB Atlas connection error:', error);
 });
 
-// Define your routes and other app logic here
+// Import user routes
+const userRoutes = require('./routes/userRoutes');
+
+// Use user routes
+app.use('/api/user', userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
